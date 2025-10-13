dbb241630fa842e3bb59cf3c8c11d1841bbbdb51
Chayuk Timofey <timon.chayuk.official@gmail.com>
2025-10-13 12:56:20 +0300

    Создан Модуль Распознавания / recognizer.py Создана проверка на wake word / wake_word_tester.py Обновление requirements.txt, config.py

---
+++ core/wake_word_tester.py
@@ -0,0 +1,49 @@
+import struct
+import time
+import pvporcupine
+import pyaudio
+from playsound import playsound
+from config import ACCESS_KEY, WAKE_WORD, BEEP_WAKE
+
+
+def play_beep():
+    try:
+        playsound(str(BEEP_WAKE[1]), block=False)
+    except Exception as e:
+        print(f"⚠️ Ошибка воспроизведения звука: {e}")
+
+def main():
+
+
+    porcupine = pvporcupine.create(
+        access_key=ACCESS_KEY,
+        keyword_paths= [WAKE_WORD]
+    )
+
+    pa = pyaudio.PyAudio()
+    stream = pa.open(
+        rate=porcupine.sample_rate,
+        channels=1,
+        format=pyaudio.paInt16,
+        input=True,
+        frames_per_buffer=porcupine.frame_length
+    )
+
+    print("Скажи - Себастьян!!")
+
+    while True:
+        pcm = stream.read(porcupine.frame_length)
+        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+        start = time.perf_counter()
+        result = porcupine.process(pcm)
+        end = time.perf_counter()
+
+        latency = (end - start) * 1000  # в миллисекундах
+
+        if result >= 0:
+            play_beep()
+            print(f"✅ Активировано! Время реакции: {latency:.2f} мс")
+
+if __name__ == "__main__":
+    main()
